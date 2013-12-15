35bbd571e3faff2a0faa65c9ca8be3398a86ae9f
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-15 19:03:26 +0400

    Error: The basic error in the axy hierarchy

---
+++ Error.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors;
+
+/**
+ * The basic error in the axy hierarchy
+ *
+ * @author Oleg Grigoreiv <go.vasac@gmail.com>
+ */
+interface Error
+{
+}
