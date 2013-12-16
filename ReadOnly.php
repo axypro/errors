d3859f54a6fd1631d57988ff9c089f422184421f
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-16 08:27:10 +0400

    ReadOnly: Trying to change readonly value

---
+++ ReadOnly.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors;
+
+/**
+ * Trying to change readonly value
+ *
+ * @author Oleg Grigoreiv <go.vasac@gmail.com>
+ */
+interface ReadOnly extends Forbidden
+{
+}
