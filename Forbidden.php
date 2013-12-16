1ae5c4cd8f639038ea3458893988da80e226c8e6
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-16 08:25:23 +0400

    Forbidden: This action is forbidden

---
+++ Forbidden.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors;
+
+/**
+ * This action is forbidden
+ *
+ * @author Oleg Grigoreiv <go.vasac@gmail.com>
+ */
+interface Forbidden extends Error
+{
+}
