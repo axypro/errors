63a2b3f0477231fa5a34bc47686d370b129207eb
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-24 15:25:57 +0400

    Init: The error to do with the initialization of an object

---
+++ Init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors;
+
+/**
+ * The error to do with the initialization of an object
+ *
+ * @author Oleg Grigoreiv <go.vasac@gmail.com>
+ */
+interface Init extends Error
+{
+}
