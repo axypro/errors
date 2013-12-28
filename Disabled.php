1333b3a5277e10c039898c810223b59032557924
Oleg Grigoriev <go.vasac@gmail.com>
2013-12-28 16:30:02 +0400

    Disabled: This service is disabled in current environment

---
+++ Disabled.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors;
+
+/**
+ * This service is disabled in current environment
+ *
+ * @author Oleg Grigoreiv <go.vasac@gmail.com>
+ */
+class Disabled extends Logic implements Forbidden
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $defaultMessage = '{{ service }} is disabled';
+
+    public function __construct($service, \Exception $previous = null, $thrower = null)
+    {
+        $this->service = $service;
+        $message = [
+            'service' => $service,
+        ];
+        parent::__construct($message, 0, $previous, $thrower);
+    }
+
+    /**
+     * Get the disabled service
+     *
+     * @return mixed
+     */
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * @var mixed
+     */
+    private $service;
+}
---
+++ tests/DisabledTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package axy\errors
+ */
+
+namespace axy\errors\tests;
+
+use axy\errors\Disabled;
+
+/**
+ * @coversDefaultClass axy\errors\Disabled
+ */
+class DisabledTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers ::__construct
+     */
+    public function testCreate()
+    {
+        $previous = new \RuntimeException('msg');
+        $e = new Disabled('Srv', $previous);
+        $this->assertInstanceOf('Exception', $e);
+        $this->assertInstanceOf('LogicException', $e);
+        $this->assertInstanceOf('axy\errors\Error', $e);
+        $this->assertInstanceOf('axy\errors\Logic', $e);
+        $this->assertInstanceOf('axy\errors\Forbidden', $e);
+        $this->assertSame($previous, $e->getPrevious());
+        $this->assertSame('Srv is disabled', $e->getMessage());
+    }
+}
